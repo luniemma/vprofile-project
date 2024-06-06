8f695ff201c11470b7067dd1dcc535e37bdbe9bd
luniemma <luniyisiemmanuel@gmail.com>
2024-06-06 14:55:26 -0400

    Update jenkins-setup.sh and added docker script and maven

--- userdata/jenkins-setup.sh
+++ userdata/jenkins-setup.sh
@@ -12,4 +12,32 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
 
 sudo apt-get update
 sudo apt-get install jenkins -y
-###
+# Update package lists
+sudo apt-get update
+
+# Install necessary packages
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+# Add Docker's official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Add Docker repository
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+# Install Docker Engine
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Add jenkins user to the docker group
+sudo usermod -aG docker jenkins
+
+# Install Maven
+sudo apt-get install -y maven
